2aeb268cee34f4ae4a0bd2fea127aa613d9c3df7
zisongz <165632513+zisongz@users.noreply.github.com>
2024-04-02 16:01:58 +0800

    Create songxiaowei.cu

---
+++ songxiaowei.cu
@@ -0,0 +1,16 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+__global__ void printHello()
+{
+    printf("宋晓维提交\n");
+    printf("Hello\n");
+}
+
+int main()
+{
+    printHello << <1, 8 >> > ();
+    cudaDeviceSynchronize(); 
+    return 0;
+}
