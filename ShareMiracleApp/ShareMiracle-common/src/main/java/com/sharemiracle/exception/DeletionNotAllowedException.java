305f5562eebbcb579b8144d5feebd7c5e8433e49
LiuTujun <liutujun2001@gmail.com>
2024-04-08 16:57:20 +0800

    initial demo

---
+++ ShareMiracleApp/ShareMiracle-common/src/main/java/com/sharemiracle/exception/DeletionNotAllowedException.java
@@ -0,0 +1,9 @@
+package com.sharemiracle.exception;
+
+public class DeletionNotAllowedException extends BaseException {
+
+    public DeletionNotAllowedException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ ShareMiracleApp/ShareMiracle-common/src/main/java/com/sharemiracle/exception/UserNotLoginException.java
@@ -0,0 +1,12 @@
+package com.sharemiracle.exception;
+
+public class UserNotLoginException extends BaseException {
+
+    public UserNotLoginException() {
+    }
+
+    public UserNotLoginException(String msg) {
+        super(msg);
+    }
+
+}
