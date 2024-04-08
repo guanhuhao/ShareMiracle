cbd3ce1b33c7436927d17f5c298b9cda047d439e
LiuTujun <liutujun2001@gmail.com>
2024-04-08 13:45:35 +0800

    initial demo
    Liu tujun

--- sunyd.java
+++
@@ -1,5 +0,0 @@
-public class sunyd {
-    public static void main(String[] args) {
-        System.out.println("孙滢栋第一次提交");
-    }
-}
--- yy.java
+++
@@ -1,5 +0,0 @@
-public class yy {
-    public static void main(String[] args) {
-        System.out.println("叶宇第一次提交");
-    }
-}
