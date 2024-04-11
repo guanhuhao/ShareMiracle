9d34eb452029d5fe09c08b6dffcb9013776a719e
syd <sunyd1999@gmail.com>
2024-04-11 16:49:37 +0800

    数据集相关接口

---
+++ ShareMiracleApp/ShareMiracle-pojo/src/main/java/com/sharemiracle/dto/DatasetDTO.java
@@ -0,0 +1,28 @@
+package com.sharemiracle.dto;
+
+import com.sharemiracle.entity.Organization;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+public class DatasetDTO implements Serializable {
+
+    //模型数据id
+    private Long id;
+
+    //模型名字
+    private String name;
+
+    //模型是否公开（1公开 0私有）
+    private boolean isPublic;
+
+    //模型存储url
+    private String modelUrl;
+
+    // 组织列表（标明哪些组织有权限使用该数据，该组织需创建者在内）
+    private List<Organization> shareOrganization = new ArrayList<Organization>();
+}
---
+++ ShareMiracleApp/ShareMiracle-pojo/src/main/java/com/sharemiracle/dto/DatasetQueryDTO.java
@@ -0,0 +1,12 @@
+package com.sharemiracle.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class DatasetQueryDTO implements Serializable {
+
+    private Long id;
+
+}
